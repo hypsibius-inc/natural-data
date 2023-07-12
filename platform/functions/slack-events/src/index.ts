9245860d9f4c55300fe5b869ee1ea3981d28b40b
jonirap <joni.rapoport@gmail.com>
2023-07-11 22:25:24 -0600

    Communication between funcs over rabbit

--- platform/functions/slack-events/src/index.ts
+++ platform/functions/slack-events/src/index.ts
@@ -2,9 +2,40 @@ import { Context, StructuredReturn } from '@hypsibius/faas-js-runtime';
 import { App } from '@slack/bolt';
 import FaaSJSReceiver from './faas-js.receiver';
 import { SlackLogger } from './slack.logger';
+import { DaprClient } from '@dapr/dapr';
+import { CloudEvent, Emitter, emitterFor, httpTransport } from 'cloudevents';
 
 let receiver: FaaSJSReceiver;
 let app: App;
+
+const K_SINK: string = process.env.K_SINK!;
+const SOURCE: string = process.env.K_REVISION!;
+if (!K_SINK) {
+  throw Error('No K_SINK env');
+}
+if (!SOURCE) {
+  throw Error(JSON.stringify(Object.keys(process.env)));
+}
+Emitter.on("cloudevent", async (ce) => {
+  console.log(`
+  Emitting ${JSON.stringify(ce)}
+  to ${K_SINK}
+  `);
+  return await emitterFor(httpTransport(K_SINK))(ce);
+})
+
+const emit = async <T>(event: string, data: T): Promise<CloudEvent<T>> => {
+  return await new CloudEvent<T>({type: event, source: SOURCE, data: data}).emit(true);
+}
+
+const dapr = new DaprClient();
+
+const publish = async (topic: string, data: object | string) => {
+  const ret = await dapr.pubsub.publish('default-pubsub', topic, data);
+  if (ret.error) {
+    throw ret.error;
+  }
+};
 
 function initialize(context: Context): FaaSJSReceiver {
   if (!receiver && !app) {
@@ -22,7 +53,9 @@ function initialize(context: Context): FaaSJSReceiver {
     });
 
     app.event('app_home_opened', async ({ logger, payload }) => {
-      logger.error(JSON.stringify(payload));
+      logger.warn(JSON.stringify(payload));
+      await publish('slack.app-home-opened', payload);
+      await emit(payload.type, payload);
     });
   }
   return receiver;
