f1034ace570b71c8744dea1ba1af958d02e87129
jonirap <joni.rapoport@gmail.com>
2023-07-19 19:54:29 -0600

    Change local packages to tarballs

--- platform/functions/update.bash
+++ platform/functions/update.bash
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 for dir in $SCRIPT_DIR/*/;
-    do (npm --prefix $dir run install-utils); 
+    do (npm --prefix $dir run install-utils);
 done;
